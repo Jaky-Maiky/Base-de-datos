2227bdebbd8792b99e653b922a4e3951cc527438
Jhonny Guevara <jhonny.guevara@pucese.edu.ec>
2020-06-12 07:29:53 -0500

    Update Cliente_mayor_cantidad_compras.sql

--- Tarea/Cliente_mayor_cantidad_compras.sql
+++ Tarea/Cliente_mayor_cantidad_compras.sql
@@ -1,19 +1,14 @@
-CREATE VIEW Cliente_mayor_cantidad_compras AS
-    SELECT 
-        COUNT(o.customerNumber) AS cantVentas,
-        c.customerName AS customerName,
-        SUM(od.quantityOrdered * od.priceEach) AS VentaTotal,
-        COUNT(DISTINCT od.productCode) AS ProductosDiferentes
-    FROM
-        ((classicmodels.customers c
-        JOIN classicmodels.orders o)
-        JOIN classicmodels.orderdetails od)
-    WHERE
-        c.customerNumber = o.customerNumber
-            AND o.orderNumber = od.orderNumber
-            AND o.orderDate BETWEEN CAST('2003-05-31' AS DATE) AND CAST('2004-05-31' AS DATE)
-    GROUP BY c.customerNumber
-    ORDER BY COUNT(o.customerNumber) DESC
-    LIMIT 1
-    
-    
+DELIMITER $$ DROP PROCEDURE IF EXISTS clientes_con_mas_compras;
+CREATE PROCEDURE clientes_con_mas_compras(
+in fecha_inicio date,
+in fecha_fin date
+)
+BEGIN select customerName, count(o.orderNumber) as total_compras, count(distinct p.productName) as total_productos
+from orderdetails od, products p, orders o, customers c
+where od.productCode = p.productCode
+and od.orderNumber = o.orderNumber
+and o.customerNumber = c.customerNumber
+and o.orderDate between fecha_inicio and fecha_fin
+group by customerName
+order by total_compras desc; END $$ 
+DELIMITER ;
